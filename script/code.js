909b62ee5b281db6dd5e93f65a49f1947ebe7525
Lucas Nuernberg <lucasnogoncalves@hotmail.com>
2022-04-28 15:08:01 -0300

    enviar

---
+++ script/code.js
@@ -0,0 +1,24 @@
+function redirecionarShops(id) {
+    switch (id) {
+        case 1:
+            window.location.href = "https://www.hakon.com.br/MLB-2625190030-kit-cruze-boto-abrir-porta-mala-chave-canivete-chevrolet-_JM?searchVariation=174448321445#searchVariation=174448321445&position=11&search_layout=stack&type=item&tracking_id=3c68da6c-f30f-4dde-b0d1-d38b09397251";
+            break;
+        case 2:
+            window.location.href = "https://www.hakon.com.br/MLB-2625198125-kit-cruze-boto-abrir-porta-mala-carcaca-chave-moderna-_JM?searchVariation=174448781943#searchVariation=174448781943&position=33&search_layout=stack&type=item&tracking_id=3c68da6c-f30f-4dde-b0d1-d38b09397251";
+            break;
+        case 3:
+            window.location.href = "https://www.hakon.com.br/MLB-1854681617-new-carcaca-chave-canivete-3-botoes-gm-cruze-lt-2012-a-2018-_JM#position=8&search_layout=grid&type=item&tracking_id=64833845-1101-4f4c-95a8-963060b48d62";
+            break;
+        case 4:
+            window.location.href = "https://www.hakon.com.br/MLB-1803206146-capa-chave-canivete-2-botoes-cruze-2013-novo-_JM";
+            break;
+        case 5:
+            window.location.href = "https://www.hakon.com.br/MLB-1776626340-carcaca-chave-canivete-cruze-20122018-novo-_JM";
+            break;
+        case 6:
+            window.location.href = "https://www.hakon.com.br/MLB-1923421796-nova-chave-2-botoes-s10-spin-sonic-cruze-montana-onix-prisma-_JM";
+            break;            
+        default:
+            break;
+    }
+}
